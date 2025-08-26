bbabaea3b0d74413c18c205589621eff79bf632c
Asyraaf Razak <m.asyraaf.ra2ak@gmail.com>
2025-08-26 17:01:08 +0800

    add function crud - inventory

--- app/Http/Controllers/InventoryController.php
+++ app/Http/Controllers/InventoryController.php
@@ -15,4 +15,24 @@ class InventoryController extends Controller
         // return to view with $inventories (resources/views/inventories/index.blade.php)
         return view('inventories.index', compact('inventories'));
     }
+
+    public function create()
+    {
+        return view('inventories.create');
+    }
+
+    public function store(Request $request)
+    {
+        //store in the table 'inventories' using model
+
+        //POPO - plain old php object
+        $inventory = new Inventory();
+        $inventory->name = $request->name;
+        $inventory->quantity = $request->quantity;
+        $inventory->description = $request->description;
+        $inventory->save();
+
+        //return to inventory index
+        return redirect('inventories');
+    }
 }
